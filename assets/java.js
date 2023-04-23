bb11dfa19344c0c6293655fe65e74b4beff11364
Sarahsegla <sarahsegla@icloud.com>
2023-04-23 17:32:47 +0100

    own slide with comments. need styling

--- assets/java.js
+++ assets/java.js
@@ -1,28 +1,48 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+// const prev = document.getElementsByClassName('prev');
+// const next = document.getElementsByClassName('next');
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// prev.addEventListener("click", function(event) {
+
+// l/\
+// })
+
+// starting from 0. array of images that will be loope through
+var i = 0;
+
+// creating an empty array of the images
+var images = [];
+
+// time that will be counted in miller seconds between each image
+var time = 3000;
+
+
+//list of the images. filling the emppty images array
+images[0] = './assets/images/ice.jpg';
+images[1] = './assets/images/mix.jpg';
+images[2] = './assets/images/smoke.jpg';
+images[3] = './assets/images/spag.png';
+images[4] = './assets/images/spag2.jpg';
+
+// function that changes the images in the array
+function imgChange() {
+    // getting mySlides from html (img name="mySlides") and getting the source images to [i] which is 0 from (var i = 0;)
+    document.mySlides.src = images[i];
+
+    // if statement
+    // if [i] is bigger than the array of the images take away one
+    if (i < images.length - 1 ){
+        // if its true incriment (increase) by 1 
+        i++;
+
+    } else {
+        //if not reset to 0
+        i = 0;
+    }
+
+    // to run the function for a sorten amount of seconds 
+    // getting time from the variable time
+    setTimeout("imgChange()", time)
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+//want the images to run when the page loads
+window.onload = imgChange;
